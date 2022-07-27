c3f8748660a276b649e269b12302c7403cab6180
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-07-27 17:02:46 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,18 @@
 import styles from './Header.module.css';
-
+import { useContext } from 'react';
+import { useContextModal } from '../../context/contextCadastro';
 
 import educaForumLogo from '../../assets/images/educaforum-logo.png';
 
-
 export function Header() {
-  const {isLoggedin} = useContext(useContextModal)
+  const { isLoggedin } = useContext(useContextModal);
   return (
     <header className={styles.header}>
       <img src={educaForumLogo} alt="Logotipo do EducaFórum" />
 
-      <a className={styles.loginBtn}href="/login">
+      <a className={styles.loginBtn} href="/login">
         <div>Login</div>
       </a>
-
     </header>
   );
 }
