ebd2844003bcf56e1811748e7da5bbca75423eee
rucp <rucp33@gmail.com>
2022-07-28 17:05:17 -0300

    feat: adding and anchoring the new logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import styles from './Header.module.css';
 
-
 import educaForumLogo from '../../assets/images/educaforum-logo.png';
 import { useContext } from 'react';
 import { useContextModal } from '../../context/contextCadastro';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 export function Header() {
   const { isLoggedin } = useContext(useContextModal);
   return (
-    <header className={styles.header}>
-      <img src={educaForumLogo} alt="Logotipo do EducaFórum" />
+    <Router>
+      <header className={styles.header}>
+        <Link to="/">
+          <img src={educaForumLogo} alt="Logotipo do EducaFórum" />
+        </Link>
 
-      <div className={styles.loginBtn}>
-        {isLoggedin ? <a href="/">Sair</a> : <a href="/login">Login</a>}
-      </div>
-
-    </header>
+        <div className={styles.loginBtn}>
+          {isLoggedin ? <a href="/">Sair</a> : <a href="/login">Login</a>}
+        </div>
+      </header>
+    </Router>
   );
 }
