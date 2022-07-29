a6e9c98a89c99b5fc5ef22c85d291f774705d35b
rucp <rucp33@gmail.com>
2022-07-28 21:11:57 -0300

    feat: adding the search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import educaForumLogo from '../../assets/images/educaforum-logo.png';
 import { useContext } from 'react';
 import { useContextModal } from '../../context/contextCadastro';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { SearchOutlined } from '@ant-design/icons';
 
 export function Header() {
   const { isLoggedin } = useContext(useContextModal);
@@ -13,6 +14,13 @@ export function Header() {
         <a href="/">
           <img src={educaForumLogo} alt="Logotipo do EducaFórum" />
         </a>
+
+        <form action="" className={styles.searchBar}>
+          <input type="text" placeholder="faça sua busca" name="" />
+          <button type="button" className={styles.btnSearch}>
+            <SearchOutlined />
+          </button>
+        </form>
 
         <div className={styles.loginBtn}>
           {isLoggedin ? <a href="/">Sair</a> : <a href="/login">Login</a>}
